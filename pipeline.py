67413ae19828f724f868d08cb9d6d4778c6656ce
Tô Viết Anh <68630777+anhtv26062000@users.noreply.github.com>
2020-12-05 13:08:40 +0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -126,5 +126,5 @@ if __name__ == '__main__':
 
         file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
 
-    data.to_csv('/content/Pipeline/data.csv', sep = ',', na_rep='Unknown')
+    data.to_csv('/content/data.csv', sep = ',', na_rep='Unknown')
     print("elapsed time : {}s".format(time.time() - t))
